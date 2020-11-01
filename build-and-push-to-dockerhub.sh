6e3e7fabbdc5ff72992859667cfe0656aa26fd24
Torsten Bøgh Köster <tbk@thiswayup.de>
2020-11-01 17:39:00 +0100

    [travis] reorganize multi-arch builds

---
+++ build-and-push-to-dockerhub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" == "master" ]; then
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+    DOCKER_BUILD_ARGS="--push"
+else
+    echo "Building multi-arch build but will not push to Docker Hub ..."
+fi
+
+docker buildx create --use --name geoip-builder node-amd64
+docker buildx create --append --name geoip-builder node-arm64
+docker context ls
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    ${DOCKER_BUILD_ARGS} \
+    --build-arg MAXMIND_LICENSE_KEY=${MAXMIND_LICENSE_KEY} \
+    -t observabilitystack/geoip-api:latest \
+    -t observabilitystack/geoip-api:$(date +%Y-%V) .
+docker buildx imagetools inspect observabilitystack/geoip-api:latest
